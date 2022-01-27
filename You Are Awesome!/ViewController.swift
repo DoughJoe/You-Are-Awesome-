bdf862ba1ccaedd94f89a7a7d847d5e325b9b9fb
Joey Moore <jjmoo@ymail.com>
2022-01-27 12:22:23 -0800

    Different messages with images.

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -13,14 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber = 0
+    var messageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func messageButtonPress(_ sender: Any) {
+        let messages = ["You are Awesome!",
+                        "You are Great!",
+                        "You are Fab!",
+                        "You Good!"]
         
-        print(imageNumber)
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
+        }
+            
         // let imageName = "image" + String(imageNumber)
         let imageName = "image\(imageNumber)"
         imageView.image = UIImage(named: imageName)
